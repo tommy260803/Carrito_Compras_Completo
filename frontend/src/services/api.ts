34feea58bb68da21f6e3fc17ccf2b0e9db0d8e97
AnthonyGarcia <agarciave@unitru.edu.pe>
2026-04-29 19:43:33 -0500

    new variable

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://carrito-compras-complete.onrender.com/api/v1';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
